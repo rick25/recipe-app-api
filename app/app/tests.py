194131c776dba9c7bdd800fb70fb894b4770d2ce
ricardoch <rchinchay@sugit.com.ar>
2023-02-26 13:54:32 -0300

    added wait_for_db command

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,8 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     def test_add_number(self):
         res = calc.add(5, 6)
-        self.assertEqual(res, 12)
+        self.assertEqual(res, 11)
